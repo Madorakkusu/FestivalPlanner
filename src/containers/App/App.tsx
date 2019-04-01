7fa1850a0283ec1eeaa660777ca90ec4d8a5cc1e
Madorakkusu <yanistam@hotmail.fr>
2019-04-01 03:59:39 +0200

    Few more config fixes

--- src/containers/App/App.tsx
+++ src/containers/App/App.tsx
@@ -1,12 +1,13 @@
 import React, { Component } from 'react';
-import { BrowserRouter, Route, Link } from "react-router-dom";
+import { BrowserRouter, Route, Link } from 'react-router-dom';
 
 export class App extends Component<any, any> {
- constructor(props) {
+  constructor(props) {
     super(props);
- }
+    console.log('true');
+  }
 
- render() {
-  return <h1>Bienvenue sur l'application Festival Planner</h1>;
- }
+  render() {
+    return <h1>Bienvenue sur l'application Festival Planner</h1>;
+  }
 }
